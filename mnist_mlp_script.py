345fdc058928099fb8fb97cf99f9a7385f24869d
Matthew Rose <jmatthew317@Hotmail.com>
2025-06-27 13:51:43 -0400

    verification checks added to script

--- mnist_mlp_script.py
+++ mnist_mlp_script.py
@@ -10,7 +10,7 @@ import numpy as np
 import csv
 import pickle
 
-
+# Importing and prepping the dataset
 def load_mnist_csv(filepath):
     with open(filepath, 'r') as f:
         reader = csv.reader(f)
@@ -21,9 +21,20 @@ def load_mnist_csv(filepath):
     y = data[:, 0].astype(int) # create the Label
     return x, y
 
+# Splitting up the dataset into their x and y sets
+X_train, y_train = load_mnist_csv("../micrograd_MLP_data/MNIST_CSV/mnist_train.csv")
+X_test, y_test = load_mnist_csv("../micrograd_MLP_data/MNIST_CSV/mnist_test.csv")
 
-X_train, y_train = load_mnist_csv("../micrograd_MLP_data/MNIST_CSV/mnist_train.csv:")
-X_test, y_test = load_mnist_csv("../micrograd_MLP_data/MNIST_CSV/mnist_train.csv")
+# Verify that the datasets have the right shape 
+# print("Training set:", X_train.shape, y_train.shape)
+# print("Test set:", X_test.shape, y_test.shape)
 
-print("Training set:", X_train.shape, y_train.shape)
-print("Test set:", X_test.shape, y_train.shape)
+# Initialize the model based off the dataset parameters
+model = MLP(784, [128, 64, 10])
+
+# Test the steps and show the initial output
+# sample_input = [Value(v) for v in X_train[0]]
+# output = model(sample_input)
+# print("Model output:", [o.data for o in output])
+# print("True lable:", y_train[0])
+
